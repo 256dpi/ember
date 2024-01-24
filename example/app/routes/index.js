8e080c87ce54c3bc12e3e521fa496632875d37f2
Joël Gähwiler <joel.gaehwiler@gmail.com>
2024-01-24 15:19:31 +0100

    updated example app

---
+++ example/app/routes/index.js
@@ -0,0 +1,11 @@
+import Route from "@ember/routing/route";
+
+export default class extends Route {
+  model() {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        resolve({ message: "Hello world!" });
+      }, 500);
+    });
+  }
+}
