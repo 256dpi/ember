8843d52fc02e191c84e447de738a7109d46a3c20
Joël Gähwiler <joel.gaehwiler@gmail.com>
2024-02-16 14:01:37 +0100

    fastboot: check if instance is running

--- fastboot/instance.go
+++ fastboot/instance.go
@@ -283,57 +283,69 @@ func (i *Instance) Visit(url string, r Request) (Result, error) {
 	// run application
 	actions = append(actions, chromedp.Evaluate(`
 		(async () => {
-			`+headersClass+`
+			try {
+				`+headersClass+`
 	
-			if (window.$instance) {
-				await $instance.destroy();
-			}
-	
-			let removeAttributes = (node) => {
-				while(node.attributes.length > 0) {
-					node.removeAttribute(node.attributes[0].name);
+				if (window.$running) {
+					throw new Error('instance running');
 				}
+	
+				if (window.$instance) {
+					await $instance.destroy();
+				}
+	
+				window.$running = true;
+		
+				let removeAttributes = (node) => {
+					while(node.attributes.length > 0) {
+						node.removeAttribute(node.attributes[0].name);
+					}
+				}
+		
+				document.head.innerHTML = '';
+				document.body.innerHTML = '';
+				removeAttributes(document.head);
+				removeAttributes(document.body);
+				removeAttributes(document.documentElement);
+		
+				window.$instance = await $app.buildInstance();
+		
+				const request = `+string(data)+`;
+				request.headers = new FastBootHeaders(request.headers);
+				request.host = () => request.headers.get('host');
+		
+				const info = {
+					request: request,
+					response: {
+						headers: new FastBootHeaders({}),
+						statusCode: 200,
+					},
+					metadata: {},
+					deferredPromise: Promise.resolve(),
+					deferRendering(promise) {
+						this.deferredPromise = promise;
+					},
+				};
+		
+				$instance.register('info:-fastboot', info, { instantiate: false });
+		
+				const options = {
+					document: window.document,
+					isBrowser: true,
+					isInteractive: false,
+					rootElement: window.document.body,
+				};
+		
+				await $instance.boot(options);
+				await $instance.visit('`+url+`', options);
+				await info.deferredPromise;
+		
+				return info;
+			} catch (err) {
+				throw err;
+			} finally {
+				window.$running = false;
 			}
-	
-			document.head.innerHTML = '';
-			document.body.innerHTML = '';
-			removeAttributes(document.head);
-			removeAttributes(document.body);
-			removeAttributes(document.documentElement);
-	
-			window.$instance = await $app.buildInstance();
-	
-			const request = `+string(data)+`;
-			request.headers = new FastBootHeaders(request.headers);
-			request.host = () => request.headers.get('host');
-	
-			const info = {
-				request: request,
-				response: {
-					headers: new FastBootHeaders({}),
-					statusCode: 200,
-				},
-				metadata: {},
-				deferredPromise: Promise.resolve(),
-				deferRendering(promise) {
-					this.deferredPromise = promise;
-				},
-			};
-	
-			$instance.register('info:-fastboot', info, { instantiate: false });
-	
-			const options = {
-				document: window.document,
-				isBrowser: true,
-				isInteractive: false,
-				rootElement: window.document.body,
-			};
-	
-			await $instance.boot(options);
-			await $instance.visit('`+url+`', options);
-			await info.deferredPromise;
-	
-			return info;
 		})()
 	`, nil, func(params *runtime.EvaluateParams) *runtime.EvaluateParams {
 		params.AwaitPromise = true
